fc585440bb18994a27315527b6f703b24b5c402f
me-piro-786 <87492539+me-piro-786@users.noreply.github.com>
2021-08-04 23:56:41 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,4 +2,6 @@ git clone https://github.com/me-piro-786/tgvc-userbot /root/TGVC
 
 cp musicplayer/.env /root/TGVC 
 cd /root/TGVC 
-cd /root/TGVC 
+docker build -t tgcalls .
+
+docker run -it --rm --env-file ./envfile --name tgvc-userbot tgcalls
