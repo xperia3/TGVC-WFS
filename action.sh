b1c02759464bfca2bd7e6a05761b8148d7076d35
xperia3 <88239736+xperia3@users.noreply.github.com>
2021-08-10 10:03:03 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/xperia3/wfs /root/vcbot
-cp musicplayer/.env /root/vcbot
-cd /root/vcbot
+git clone https://github.com/TeamDaisyX/DaisyXMusic /root/DaisyXMusic
+cp musicplayer/.env /root/DaisyXmusic
+cd /root/DaisyXmusic
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t musicplayer
 docker run --privileged --env-file .env --rm -i musicplayer 
