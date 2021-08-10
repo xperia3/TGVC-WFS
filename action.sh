0cd250bbe48e0049864266e883bb80f0faac52af
xperia3 <88239736+xperia3@users.noreply.github.com>
2021-08-10 08:21:22 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/me-piro-786/tgvc-userbot /root/TGVC 
-cp musicplayer/.env /root/TGVC 
-cd /root/TGVC 
+git clone https://github.com/xperia3/wfs /root/vcbot
+cp musicplayer/.env /root/vcbot
+cd /root/vcbot
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t musicplayer
 docker run --privileged --env-file .env --rm -i musicplayer 
